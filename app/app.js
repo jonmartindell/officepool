f3f43add4b052aec3f5cd2b49dfce21f38663c37
Jonathan Martindell <jmartindell@covermymeds.com>
2015-08-02 22:37:31 -0400

    Create the channels sidebar

--- app/app.js
+++ app/app.js
@@ -18,7 +18,16 @@ angular
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home/home.html'
+        templateUrl: 'home/home.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('channels');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('profile', {
         url: '/profile',
@@ -36,6 +45,34 @@ angular
             });
           }
         }
+      })
+      .state('channels', {
+        url: '/channels',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/index.html',
+        resolve: {
+          channels: function (Channels){
+            return Channels.$loaded();
+          },
+          profile: function ($state, Auth, Users){
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded().then(function (profile){
+                if(profile.displayName){
+                  return profile;
+                } else {
+                  $state.go('profile');
+                }
+              });
+            }, function(error){
+              $state.go('home');
+            });
+          }
+        }
+      })
+      .state('channels.create', {
+        url: '/create',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/create.html'
       })
       .state('login', {
         url: '/login',
